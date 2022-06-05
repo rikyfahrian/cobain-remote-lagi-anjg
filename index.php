6817be702d5a89cf20595c4b1084e36eb0d59abb
rikyfahrian <rikyfhrian@gmail.com>
2022-06-05 09:48:19 +0700

    nambah list

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 <body>
     <h1>bye, world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores architecto suscipit deserunt eius nihil veritatis facere hic, neque totam, nam odio nesciunt maxime placeat voluptates! Numquam possimus maiores praesentium explicabo ab repudiandae temporibus vero, at sequi reprehenderit. Suscipit eum facilis, doloribus delectus soluta error recusandae, ducimus aut voluptatum libero alias.</p>
+
+
+    <ul>
+        <li>jangan bohong</li>
+        <li>aku tidak bohong</li>
+    </ul>
 </body>
 </html>
